eba898d9a1ffb152641df4157be547e2d31f6923
AYAKO\綾子 <ayako.sabu@gmail.com>
2018-03-12 10:05:42 +0900

    2018/03/11

--- Practice/src/practice10/PTra10_03.java
+++ Practice/src/practice10/PTra10_03.java
@@ -15,28 +15,41 @@ public class PTra10_03 {
 	public static void main(String[] args) {
 
 		// Userクラスを作成後に着手してください
-		// ★ User型の変数usを宣言してください
+		User us;// ★ User型の変数usを宣言してください
 
 
-		// ★ 変数usに、Userクラスのインスタンスを作成・代入してください
+		User us1 = new User();// ★ 変数usに、Userクラスのインスタンスを作成・代入してください
 
 
 		System.out.println("ユーザID（数字）、 ユーザ名、メールアドレス、パスワードをカンマ区切りで入力してください");
 
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
 		// ★ java.util.Scannerクラスをインスタンス化し、変数scannerに代入してください
-
+		
+		
+		// ※ コマンドプロンプトで入力された値が、数字ではなかった場合はプログラムがエラーになります
 
 		// ★ 入力された値を、String型の変数lineに格納してください
-
+		String line = scanner.nextLine();
 
 		// ★ 変数lineに格納されている値を','で区切り、配列にしてください（Stringクラスのメソッドを使用します）
+		String[] list = line.split(",",0);
 
 
+		us1.userId = Integer.parseInt(list[0]);
+		us1.userNm = list[1];
+		us1.mail   = list[2];
+		us1.password = list[3];
+		
 		// ★ 配列にした値を、usの各フィールドに代入してください
 		// ユーザIDに該当する値が数値でなかった場合は、エラーでプログラムが止まっても良いです
+		
 
 
-		// ★ 変数usに格納されているインスタンスの、フィールドuserId, userNm, mail, passwordを出力してください
+		System.out.println(us1.userId);
+		System.out.println(us1.userNm);
+		System.out.println(us1.mail);
+		System.out.println(us1.password);// ★ 変数usに格納されているインスタンスの、フィールドuserId, userNm, mail, passwordを出力してください
 
 	}
 }
--- Practice/src/practice10/PTra10_06.java
+++ Practice/src/practice10/PTra10_06.java
@@ -14,10 +14,54 @@ public class PTra10_06 {
 
 	public static void main(String[] args) {
 
+		Car[] cars = new Car[3];
+		
+		Car car1 = new Car();
+		car1.serialNo = 1;
+		car1.color = "Red";
+		car1.gasoline = 50;
+		
+		Car car2 = new Car();
+		car2.serialNo = 2;
+		car2.color = "Blue";
+		car2.gasoline = 100;
+		
+		Car car3 = new Car();
+		car3.serialNo = 3;
+		car3.color = "Yellow";
+		car3.gasoline = 200;
+		
+		final int distance = 300;
+		
 		/*
 		 * carインスタンスを3件作成し、それぞれの色、ガソリンを入力して決定してください
 		 * 各carインスタンスのrunメソッドを実行して、それぞれ「目的地にまでn時間かかりました。残りのガソリンは、xリットルです」を出力してください。
 		 */
-
+		
+		cars[0] = car1;
+		cars[1] = car2;
+		cars[2] = car3;
+		
+		for(int i = 0;i<cars.length;i++) {
+						
+			int sum = 0;
+			int count = 0;
+			
+			while(true) {
+				int run = cars[i].run();
+				sum += run;
+				count++;
+				
+				if(run == -1) {
+					System.out.println("目的地に到達できませんでした");
+					break;
+				}
+				
+				if(sum > distance) {
+					System.out.println("目的地までに"+count+"時間かかりました。残りのガソリンは"+cars[i].gasoline+"リットルです。");
+					break;
+				}
+			}
+		}
 	}
 }
